78652daf7e9b0b926ee60c6c17f935bb4cb90e77
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-05-20 15:53:05 -0500

    Aprendiendo Spring Boot

---
+++ src/main/java/com/santiagoprogrammer/Tutorial/Rutas.java
@@ -0,0 +1,71 @@
+package com.santiagoprogrammer.Tutorial;
+
+import com.santiagoprogrammer.Tutorial.models.Libro;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class Rutas {
+
+    private final Logger logger = LoggerFactory.getLogger(Rutas.class);
+
+    @GetMapping("/hola")
+    String miPrimeraRuta () {
+        return "Hola mundo desde Spring Controller :)";
+    }
+
+    @GetMapping("/libro/{id}/editorial/{editorial}")
+    String leerLibro(@PathVariable int id, @PathVariable String editorial) {
+        return "Leyendo el libro con id: " + id + ", editorial: " + editorial;
+    }
+
+    @GetMapping("/libro2/{id}")
+    String leerLibro2(@PathVariable int id, @RequestParam String gato, @RequestParam String editorial) {
+        return "Leyendo el libro con id: " + id + ", params: " + gato + ", editorial: " + editorial;
+    }
+
+    @PostMapping("/libro")
+    String guardarLibro(@RequestBody Map<String, Object> libro) {
+
+        libro.keySet().forEach(llave -> {
+            logger.debug("llave {} valor {}", llave, libro.get(llave));
+        });
+
+        return "Libro guardado";
+    }
+
+    @PostMapping("/crear_libro")
+    String guardarLibro(@RequestBody Libro libro) {
+        logger.debug("nombre {} editorial {}", libro.nombre, libro.editorial);
+
+        return "Libro guardado con éxito";
+    }
+
+    @GetMapping("/saludar")
+   //@ResponseStatus(value = HttpStatus.OK)
+    @ResponseStatus(value = HttpStatus.MOVED_PERMANENTLY, reason = "Fue movida a la version 2.0 de la API")
+    String miSegundaRutaconStatus(){
+        return "Aprendiendo Spring Boot Status:)";
+    }
+
+    @GetMapping("/animales/{lugar}")
+    public ResponseEntity<String> getAnimales(@PathVariable String lugar) {
+        if (lugar.equals("granja")) {
+            return ResponseEntity.status(HttpStatus.OK).body("Caballo, vaca, oveja, gallina");
+        } else if (lugar.equals("selva")){
+            return ResponseEntity.status(HttpStatus.OK).body("Mono, gorila, puma");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Lugar no valido");
+        }
+    }
+
+    @GetMapping("/calcular/{numero}")
+    public int getCalculo(@PathVariable int numero) {
+        throw new NullPointerException("Esta excepción ocurrio porque habla de información no relevante para el cliente.");
+    }
+}
---
+++ src/main/java/com/santiagoprogrammer/Tutorial/TutorialApplication.java
@@ -0,0 +1,28 @@
+package com.santiagoprogrammer.Tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(TutorialApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TutorialApplication.class, args);
+
+
+
+		/*
+		 logger.debug("Mi mensaje debug");
+		 logger.trace("Mi mensaje trace");
+		 logger.info("Mi mensaje info");
+		 logger.warn("Mi mensaje warn");
+		 logger.error("Mi mensaje error");
+		*/
+    }
+
+}

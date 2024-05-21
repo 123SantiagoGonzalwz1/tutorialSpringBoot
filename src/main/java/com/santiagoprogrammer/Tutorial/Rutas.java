c8f085052636b71604b1bb926662e26057ce9231
Santiago González <109437318+123SantiagoGonzalwz1@users.noreply.github.com>
2024-05-21 15:59:12 -0500

    JSON Response con Estructura de Datos

--- src/main/java/com/santiagoprogrammer/Tutorial/Rutas.java
+++ src/main/java/com/santiagoprogrammer/Tutorial/Rutas.java
@@ -1,6 +1,7 @@
 package com.santiagoprogrammer.Tutorial;
 
 import com.santiagoprogrammer.Tutorial.models.Libro;
+import com.santiagoprogrammer.Tutorial.models.UserData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -80,5 +81,10 @@ public class Rutas {
     @GetMapping("/userData/v2")
     public Map<String, Map<String, Object>> getUserDataV2() {
         return Map.of("user", Map.of("name", "mary", "age", 25));
+    }
+
+    @GetMapping("/userData/v3")
+    public UserData getUserDataV3() {
+        return new UserData("mary", 25, "Ciudad Verde");
     }
 }
